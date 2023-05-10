53bcd019e16cd1c4fc9407de35c01fc1cde22401
MarkOooss <122401954+MarkOooss@users.noreply.github.com>
2023-05-10 09:50:09 +0200

    Add files via upload

---
+++ sss/clock.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    var timeString = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+    
+    document.getElementById('clock-display').textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
